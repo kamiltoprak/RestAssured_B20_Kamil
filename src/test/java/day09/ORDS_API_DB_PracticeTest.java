4ea58839c0ee4bf8c07f5ca3c30f0bc8e92476be
kamiltoprak <ktoprak@gmail.com>
2020-12-19 11:45:05 -0500

    fifth

--- src/test/java/day09/ORDS_API_DB_PracticeTest.java
+++ src/test/java/day09/ORDS_API_DB_PracticeTest.java
@@ -7,13 +7,14 @@ import pojo.Country;
 import pojo.Region;
 import utility.DB_Utility;
 import io.restassured.response.Response;
+
+import java.util.List;
 import java.util.Map;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import TestBase.HR_ORDS_TestBase;
 public class ORDS_API_DB_PracticeTest extends HR_ORDS_TestBase {
-
 
     @DisplayName("GET /countries/{country_id} Compare Resut with DB")
     @Test
@@ -22,16 +23,51 @@ public class ORDS_API_DB_PracticeTest extends HR_ORDS_TestBase {
         // save the  result as Country POJO
 
         String myCountryID="AR";
-        given()
+       Country arPOJO= given().log().ifValidationFails()
                 .pathParam("country_id",myCountryID).
         when()
-                .get("/countries/{country_id}")
+                .get("/countries/{country_id}").prettyPeek()
                 .as(Country.class);
 
+       //here  the shorter way of above code
+       //Country arPOJO1= get("/countries/{country_id}",myCountryID).as(Country.class);
 
+        System.out.println("asPOJO = " + arPOJO);
 
+        String query="Select * from countries where Country_ID = '"+myCountryID+"'";
+        System.out.println("query = " + query);
 
+        DB_Utility.runQuery(query);
+        Map<String,String> dbResultMap=DB_Utility.getRowMap(1);
+
+        //now start validating the  actual response to  expected result from  database
+        assertThat(arPOJO.getCountry_id(),is(dbResultMap.get("COUNTRY_ID")));
+        assertThat(arPOJO.getCountry_name(),is(dbResultMap.get("COUNTRY_NAME")));
+
+        //  we convert region ID from  int  to  string  in pojo  file
+        //assertThat(arPOJO.getRegion_id()+"",equalTo(dbResultMap.get("REGION_ID")));
+        
+        //save region_id convert  to integer from string  in the the map
+        int expectedRegionID=Integer.parseInt(dbResultMap.get("REGION_ID"));
+        assertThat(arPOJO.getRegion_id(),equalTo(expectedRegionID));
 
     }
 
+
+    @DisplayName("GET /countries capture all country ID Compare Result with DB")
+    @Test
+    public void testResponseAllCountryIdsMatchDatabaseData(){
+
+        List<String> allCountriesIDs=get("/countries").jsonPath().getList("items.country_id");
+      // allCountriesIDs.forEach(System.out::println);
+
+        System.out.println("========Countries ID  from DB UTILITY==============");
+
+       DB_Utility.runQuery("select * from countries");
+       List<String> expectedListFromDB=DB_Utility.getColumnDataAsList("COUNTRY_ID");
+      // expectedListFromDB.forEach(System.out::println);
+
+       //assert both list  has same information
+        assertThat(allCountriesIDs,equalTo(expectedListFromDB));
+    }
 }
