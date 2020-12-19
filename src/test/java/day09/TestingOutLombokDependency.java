4ee04a6ecdefd9fa6a6d8574e25f13d6434b5e36
kamiltoprak <ktoprak@gmail.com>
2020-12-19 11:58:18 -0500

    sixth

--- src/test/java/day09/TestingOutLombokDependency.java
+++ src/test/java/day09/TestingOutLombokDependency.java
@@ -1,5 +1,24 @@
 package day09;
 
+import org.junit.jupiter.api.Test;
+import pojo.Department;
+
 public class TestingOutLombokDependency {
+
+    @Test
+    public void testDepartmentPOJO(){
+        Department d=new Department();
+        d.setDepartment_id(100);
+        System.out.println(d.getDepartment_id());
+
+        Department d2=new Department(100,"abc",12,1400);
+        System.out.println("d2 = " + d2);
+
+
+    }
+
+
+
+
 
 }
